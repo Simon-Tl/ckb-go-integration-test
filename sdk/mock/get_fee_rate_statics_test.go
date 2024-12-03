d4f933400e202bd0d46b37aa06f4bea90b855b38
Simon-Tl <SimonSage.Tl@gmail.com>
2024-12-03 14:58:52 +0800

    update ckb-sdk-go v2.3.0

--- sdk/mock/get_fee_rate_statics_test.go
+++ sdk/mock/get_fee_rate_statics_test.go
@@ -2,15 +2,17 @@ package mock
 
 import (
 	"context"
-	"github.com/onsi/gomega"
+	"fmt"
 	"testing"
+
+	"github.com/onsi/gomega"
 )
 
 // 传入方法名不一致mock方法get_fee_rate_statics sdk传入get_fee_rate_statistics
 
 func TestGetFeeRateStatics(t *testing.T) {
 
-	t.Run("get_fee_rate_statics/[null]", func(t *testing.T) {
+	t.Run("get_fee_rate_statistics/[null]", func(t *testing.T) {
 		g := gomega.NewGomegaWithT(t) // Initialize Gomega
 
 		println("Running test case:", t.Name()) // Identifiable marker
@@ -23,7 +25,38 @@ func TestGetFeeRateStatics(t *testing.T) {
 		mockResult, err := interfaceToMapString(mockData.Response.Result)
 		g.Expect(err).To(gomega.BeNil(), "mockResult interfaceToMapString failed")
 		// Description with marker
-		g.Expect(info.Mean).To(gomega.Equal(mockResult["mean"]),
+		LocaResult, err := interfaceToUint(mockResult["mean"])
+		g.Expect(err).To(gomega.BeNil(), "LocaResult interfaceToUint failed")
+
+		g.Expect(info.Mean).To(gomega.Equal(uint64(LocaResult)),
 			"Result Unequal") // Description added for clarity
 	})
+
+	t.Run("get_fee_rate_statistics/[target]", func(t *testing.T) {
+		g := gomega.NewGomegaWithT(t) // Initialize Gomega
+
+		println("Running test case:", t.Name()) // Identifiable marker
+		client, mockData, err := getMockRpcClientByName(t.Name())
+		g.Expect(err).To(gomega.BeNil(), "getMockRpcClientByName failed") // Identifiable description for the expectation
+
+		Params, err := interfaceSliceToStringSlice(mockData.Request.Params)
+		g.Expect(err).To(gomega.BeNil(), "Params interfaceSliceToStringSlice failed")
+
+		ParamsTarget, err := interfaceToUint(Params[0])
+		g.Expect(err).To(gomega.BeNil(), "ParamsTarget interfaceToUint failed ")
+
+		info, err := client.GetFeeRateStatistics(context.Background(), uint64(ParamsTarget))
+		fmt.Println(info)
+		g.Expect(err).To(gomega.BeNil(), "GetFeeRateStatistics failed")
+
+		mockResult, err := interfaceToMapString(mockData.Response.Result)
+		g.Expect(err).To(gomega.BeNil(), "mockResult interfaceToMapString failed")
+		// Description with marker
+		LocaResult, err := interfaceToUint(mockResult["mean"])
+		g.Expect(err).To(gomega.BeNil(), "LocaResult interfaceToUint failed")
+		fmt.Println(LocaResult)
+		g.Expect(info.Mean).To(gomega.Equal(uint64(LocaResult)),
+			"Result Unequal") // Description added for clarity
+	})
+
 }
--- sdk/mock/get_live_cell_test.go
+++ sdk/mock/get_live_cell_test.go
@@ -2,9 +2,10 @@ package mock
 
 import (
 	"context"
+	"testing"
+
 	"github.com/nervosnetwork/ckb-sdk-go/v2/types"
 	"github.com/onsi/gomega"
-	"testing"
 )
 
 func TestGetLiveCell(t *testing.T) {
@@ -56,41 +57,6 @@ func TestGetLiveCell(t *testing.T) {
 		}
 
 		info, err := client.GetLiveCell(context.Background(), &outPoint, mockData.Request.Params[1].(bool), nil)
-		g.Expect(err).To(gomega.BeNil(), "sdk GetLiveCell failed")
-
-		mockResult, err := interfaceToMapString(mockData.Response.Result)
-		g.Expect(err).To(gomega.BeNil(), "mockResult failed")
-
-		mockCellResult, err := interfaceToMapString(mockResult["cell"])
-		g.Expect(err).To(gomega.BeNil(), "mockCellResult failed")
-
-		mockCellOutPutResult, err := interfaceToMapString(mockCellResult["output"])
-
-		mockCellResultUint, err := interfaceToUint(mockCellOutPutResult["capacity"])
-
-		g.Expect(info.Cell.Output.Capacity).To(gomega.Equal(uint64(mockCellResultUint)), "Capacity Unequal")
-		g.Expect(info.Status).To(gomega.Equal(mockResult["status"].(string)), "Status Unequal")
-
-	})
-
-	t.Run("get_live_cell/[out_point,with_data=false,include_tx_pool=false]", func(t *testing.T) {
-		g := gomega.NewGomegaWithT(t)
-		println("Running test case:", t.Name())
-		client, mockData, err := getMockRpcClientByName(t.Name())
-		g.Expect(err).To(gomega.BeNil(), "getMockRpcClientByName failed")
-
-		Parms, err := interfaceSliceToMapString(mockData.Request.Params)
-		g.Expect(err).To(gomega.BeNil(), "Parms failed")
-		index, err := interfaceToUint(Parms["index"])
-		g.Expect(err).To(gomega.BeNil(), "index failed")
-		outPoint := types.OutPoint{
-			TxHash: types.HexToHash(Parms["tx_hash"].(string)),
-			Index:  uint32(index),
-		}
-
-		includeTxPool := mockData.Request.Params[2].(bool)
-
-		info, err := client.GetLiveCell(context.Background(), &outPoint, mockData.Request.Params[1].(bool), &includeTxPool)
 		g.Expect(err).To(gomega.BeNil(), "sdk GetLiveCell failed")
 
 		mockResult, err := interfaceToMapString(mockData.Response.Result)
